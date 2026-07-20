511629180d2579ff1928141256cdfd4a597327c9
Nishat-Annan-Mim <nishat.annan.mim.2003@gmail.com>
2026-07-20 22:57:10 +0600

    fix authentication api

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination:
+          "https://studysprint-aiagent-server.onrender.com/api/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
